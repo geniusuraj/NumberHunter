950ef29cb50e94b4eb7e3367b4e63668983d8422
geniusuraj <geniusurajpy@gmail.com>
2023-03-05 08:25:55 +0530

    Number Hunter

--- game.js
+++ game.js
@@ -9,14 +9,14 @@ const result = document.getElementById("result");
 // Add an event listener to the submit button
 submitGuess.addEventListener("click", function() {
   // Get the user's guess
-  const guess = parseInt(guessInput.value);
+const guess = parseInt(guessInput.value);
 
   // Check if the guess is correct
-  if (guess === randomNumber) {
+if (guess === randomNumber) {
     result.innerHTML = "Congratulations! You guessed the number!";
     setTimeout(function(){window.location.reload();},3000);
-  } else {
+} else {
     result.innerHTML = "Sorry, that's not the number. Please try again.";
     setTimeout(function(){window.location.reload();},3000);
-  }
+}
 });
